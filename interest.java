389aa125a316e4a2e8fa72d45e7a5834bf940084
sirisha00 <55032030+sirisha00@users.noreply.github.com>
2020-02-20 22:39:53 +0530

    Create interest.java

---
+++ interest.java
@@ -0,0 +1,32 @@
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class interest {
+	
+	public static void interestevaluation()
+	{
+		Scanner sc=new Scanner(System.in);
+		PrintStream pts=new PrintStream(new FileOutputStream(FileDescriptor.out));
+		
+		pts.println("Enter the principal amount :");
+		try {
+		double princpalamount=sc.nextDouble();
+		pts.println("Enter the time period :");
+		double timeperiod=sc.nextDouble();
+		pts.println("Enter the rate of interest per annum :");
+		double rateofinterest=sc.nextDouble();
+		//sc.close();
+		pts.println("Simple Interest :"+(princpalamount*timeperiod*rateofinterest)/100);
+		pts.println("Compound Interest :"+((princpalamount*Math.pow(1+(rateofinterest/100),timeperiod))-princpalamount));
+		pts.println("-------------------------------------");
+		}
+		catch (Exception e) {
+			pts.println("Enter a valid input ... ");
+			pts.println("-------------------------------------");
+			
+		}
+	}
+
+}
